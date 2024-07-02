484bee496087efac8bb51ed74c8093b8ef3016ee
JK432 <jk04032002@gmail.com>
2024-07-02 23:16:11 +0530

    fix import error

--- src/app/home/notecreate/notecreate.component.ts
+++ src/app/home/notecreate/notecreate.component.ts
@@ -1,27 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotelistComponent } from '../notelist/notelist.component';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-notecreate',
   standalone: true,
-  imports: [RouterOutlet,NotelistComponent],
+  imports: [RouterOutlet,NotelistComponent,FormsModule,CommonModule],
   templateUrl: './notecreate.component.html',
   styleUrl: './notecreate.component.css'
 })
 export class NotecreateComponent {
-  // title: string;
-  // content: string;
-  // notes: { title: string, content: string }[] = [];
+  title: string="";
+  content: string="";
+  notes: { title: string, content: string }[] = [];
 
-  // onSubmit() {
-  //   const newNote = { title: this.title, content: this.content };
-  //   this.notes.push(newNote);
-  //   console.log('Notes:', this.notes);
-  //   // Clear the form fields after submission
-  //   this.title = '';
-  //   this.content = '';
-  // }
+  onSubmit() {
+    const newNote = { title: this.title, content: this.content };
+    this.notes.push(newNote);
+    console.log('Notes:', this.notes);
+    // Clear the form fields after submission
+    this.title = '';
+    this.content = '';
+  }
 }
